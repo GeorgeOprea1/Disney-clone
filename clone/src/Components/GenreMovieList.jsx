46e452840f31235ba30c50e31005c30b437bb671
GeorgeOprea <oprea.george14@yahoo.com>
2024-03-01 18:27:28 +0200

    finished the MovieList MovieCard HrMovieCard components

--- clone/src/Components/GenreMovieList.jsx
+++ clone/src/Components/GenreMovieList.jsx
@@ -1,18 +1,25 @@
 import GenreList from "../Constant/GenreList";
+import MovieList from "./MovieList";
 
-const GenreMovieList = () => {
+function GenreMovieList() {
   return (
     <div>
       {GenreList.genere.map(
         (item, index) =>
           index <= 4 && (
-            <div key={item.id} className="p-8 px-8 md:px-16">
-              <h2 className="text-[20px] text-white font-bold">{item.name}</h2>
+            <div className="p-4 px-8 md:px-16" key={item.id}>
+              <h2
+                className="text-[20px] text-white 
+                font-bold"
+              >
+                {item.name}
+              </h2>
+              <MovieList genreId={item.id} index_={index} />
             </div>
           )
       )}
     </div>
   );
-};
+}
 
 export default GenreMovieList;
